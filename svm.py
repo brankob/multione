24e4a5717bfe493f7d07dcaa5ea50dc2657c1ed0
brankob <brankobogunovic@gmail.com>
2025-04-08 21:12:17 +0200

    Add files via upload

---
+++ svm.py
@@ -0,0 +1,142 @@
+import fiona
+import rasterio as rio
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn import preprocessing, metrics
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+import  matplotlib.pyplot as plt
+import seaborn as sns
+from osgeo import gdal
+
+#Input files
+raster_loc = "enhancened_sentinel_image.tif"
+points_loc = "ppi_points.shp"
+temp_point_loc = "temp_y_points.shp"
+lulc_name = ['1', '2', '3', '4','5']
+# Read bands from input raster
+with rio.open(raster_loc) as img:
+    bands = img.count
+print("bands of input image:", bands)
+
+# Create a list of feature names
+features = ["band" + str(i) for i in range(1,bands+1)]
+print('Bands names:', features)
+f_len=len(features)
+#Read points from shapefile
+points = gpd.read_file(points_loc)
+# Add a new column "id" with range of points
+points['id'] = range(len(points))
+# Save the new point file with 'id'
+points.to_file(temp_point_loc)
+
+# Converts GetDataFrame to DataFrame and remove the 'geometry' column
+points_df = pd.DataFrame(points.drop(columns='geometry'))
+#Initialize an empty DataFrame to store sampled values
+sampled = pd.DataFrame()
+
+#Read input shapefile with Fiona and iterate over each feature
+with fiona.open(temp_point_loc) as shp:
+    for feature in shp:
+        #print(feature)
+        siteID = feature['properties']['ppi']
+        coords = feature['geometry']['coordinates']
+        # Read pixel values at the given coordinates using Rasterio
+        with rio.open(raster_loc) as stack_src:
+            values = [v for v in stack_src.sample([coords])]
+
+        # Create a temporary DataFrame for the sampled values
+        temp_df = pd.DataFrame(values, columns=features)
+        # Set PPI data into five clasess for easier  interpretation of data
+        if (siteID <= 0.2):                      # IF PPI is smaller then 0.2 put into 1 class
+            temp_df['id'] = 1
+        elif ((siteID > 0.2) and (siteID <= 0.4)):# IF PPI is greater then 0.2  and smaller then 0.4 put into 2 class
+            temp_df['id'] = 2
+        elif ((siteID > 0.4) and (siteID <= 0.6)):# IF PPI is greater then 0.4  and smaller then 0.6 put into 3 class
+            temp_df['id'] = 3
+        elif ((siteID > 0.6) and (siteID <= 0.8)):# IF PPI is greater then 0.4  and smaller then 0.6 put into 4 class
+            temp_df['id'] = 4
+        else:
+            temp_df['id'] = 5   # IF PPI is greater then 0.8  5 class
+        sampled = sampled._append(temp_df, ignore_index=True)
+
+# SPlit the data into features (X) and labels (Y)
+X = np.ravel(sampled.iloc[:,:f_len].values)
+Y = np.round(np.tile(np.ravel(sampled.iloc[:,f_len:4].values),3))
+
+# SPlit the data into training and testing sets
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.35, stratify=Y)
+#print(f'X_train Shape:{X_train.shape}\nX_test Shape: {X_test.shape }\nY_train Shape: {Y_train.shape}\nY_test: {Y_test.shape}')
+
+# Initialize and train a SVM classifier
+clf = SVC(kernel='rbf')
+
+#Reshape train and test data to 2d array
+X_train = X_train.reshape(-1, 1)
+Y_train = Y_train.reshape(-1, 1)
+X_test = X_test.reshape(-1, 1)
+Y_train = Y_train.reshape(-1, 1)
+
+# Fit train data
+clf.fit(X_train, Y_train)
+
+#Make predictions on the test data
+Y_pred = clf.predict(X_test)
+#test = metrics.f1_score(Y_test, Y_pred, average='weighted', labels=np.unique(Y_pred))
+
+# Evaluate the classifier performance
+accuracy = accuracy_score(Y_test, Y_pred)
+report = classification_report(Y_test, Y_pred)
+confusion = confusion_matrix(Y_test, Y_pred)
+
+print(f"Accuracy: {accuracy * 100: .2f}%")
+# print(report)
+print("Confusion Matrix:\n", confusion)
+
+# Visualize the confusion matrix
+cm_percent = confusion / np.sum(confusion)
+plt.figure(figsize=(7,7), facecolor ='w', edgecolor='k')
+sns.set(font_scale=1.5)
+sns.heatmap(cm_percent, xticklabels=lulc_name, yticklabels=lulc_name, cmap="jet", annot=True, fmt='.2%',
+           cbar=False, linewidths=2,linecolor='black')
+plt.title('SVM')
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.show()
+
+# Export the prediction results as geotiff file
+out_file = "ppi_SVM.tif"
+
+
+gdal.Warp('output_small_res.tif', raster_loc, xRes=100, yRes=100)
+
+raster_loc = 'output_small_res.tif'
+with rio.open(raster_loc) as img:
+    img_arr = img.read()
+    bands = img_arr.shape[0]
+    height, width = img_arr.shape[1], img_arr.shape[2]
+    crs = img.crs
+    transform = img.transform
+
+#Reshape the input data for prediction
+print( img_arr.size)
+img_n = np.moveaxis(img_arr,0 , -1)
+print(img_n.shape)
+img_n = img_n.reshape(-1, f_len)
+
+img_n = img_n.reshape(-1, 1)
+print(img_n.size)
+# Perform predictions o the entire raster
+pred_full = clf.predict(img_n)
+
+#img_reshape = pred_full.reshape((height, width, 3))
+img_reshape = pred_full.reshape( height, width, 3)
+img_reshape = np.moveaxis(img_reshape,-1 , 0)
+print(img_reshape.size)
+#Export the results
+out_raster = rio.open(out_file, 'w',driver='GTiff', height=height, width=width, count=3,
+                      dtype='uint8', crs=crs, transform=transform, nodata=255)
+out_raster.write(img_reshape)
+out_raster.close()
