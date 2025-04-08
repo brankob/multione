310c7dba8b4cb0c524124e77cc67bf04d961c9aa
brankob <brankobogunovic@gmail.com>
2025-04-08 23:44:32 +0200

    Add files via upload

---
+++ sql/multione.sql
@@ -0,0 +1,92 @@
+DROP TABLE IF EXISTS public.points_ppi;
+
+-- Create table
+CREATE TABLE IF NOT EXISTS public.points_ppi(
+	lat double precision,
+ 	lon double precision,
+	ppi float,
+	the_geom geometry(Point, 3765)
+);
+
+-- Import file - Read ppi data 
+COPY  public.points_ppi(lon, lat, ppi )
+FROM '/tmp/points_ppi.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+-- Create index on lon & lat
+-- CREATE INDEX idx_lon_ais ON public.points_ppi USING gist(lon);
+-- CREATE INDEX idx_lat_ais ON public.points_ppi USING gist(lat);
+
+-- 2.) CALCULATE POINT from lon&lat to geometry 3765 Croatian coordinate system
+UPDATE  public.points_ppi
+    SET the_geom = ST_Transform(ST_GeomFromText('POINT(' || lon  || ' ' || lat || ')', 4326),3765);
+
+-- Remove NULL(Missing values) values from table
+DELETE FROM public.points_ppi
+WHERE lon IS NULL OR lat IS NULL OR ppi IS NULL;
+
+
+----------------------------  DEFINE -> Find min easting&northing (lat&lon) (bottom left corner)  ---------------------
+--------------------------               - for creating grid       --------------------------
+SELECT min(ST_X(ST_Centroid(the_geom))) AS min_x,
+       min(ST_Y(ST_Centroid(the_geom))) AS min_y
+	  -- min(ST_X(ST_Centroid(ST_Transform(the_point, 4326)))) AS long,
+	  -- min(ST_Y(ST_Centroid(ST_Transform(the_point, 4326)))) AS lat
+FROM public.points_ppi;
+
+
+--------------------------- Create grid     ---------------------------
+-- Drop table if exists
+DROP TABLE IF EXISTS public.ppi_grid;
+
+-- Create GRID -> test radar table 
+CREATE TABLE  public.ppi_grid(
+         the_geom geometry(multipolygon,3765)
+);
+
+-- Create index
+CREATE INDEX idx_ppi_grid ON public.ppi_grid USING gist(the_geom); 
+
+
+-- Run for creating grid - multipolygon
+-- Size X => 657*30m=19,710m
+-- Size Y => 1106*30m=33,180m 
+-- Number of cells 657*1106=726,642
+
+INSERT INTO public.ppi_grid
+SELECT ST_SetSRID(ST_Collect(cells.geom), 3765)
+FROM ST_CreateFishnet(657, 1106, 30, 30, 576633, 5032476) AS cells;
+
+
+
+-------------------------------------  Convert Grid multipolygon into  -------------------------- 
+-------------------------------------  -> N polygons  --------------------------
+-- Drop table if exists
+DROP TABLE IF EXISTS tbl_ppi_grid_n;
+
+-- Create GRID -> test radar table 
+CREATE TABLE tbl_ppi_grid_n(
+	     id serial,
+         the_geom geometry(polygon,3765)
+);
+
+-- Add index
+CREATE INDEX idx_ppi_grid_n ON tbl_ppi_grid_n USING gist(the_geom); 
+
+-- Convert multipolygon to N polygons 
+INSERT INTO tbl_ppi_grid_n (the_geom)
+SELECT ((ST_Dump(the_geom)).geom) AS geom   
+FROM ppi_grid;
+
+
+DROP TABLE IF EXISTS tbl_ppi_line;
+
+-- Create GRID -> test radar table 
+CREATE TABLE tbl_ppi_line(
+	     id serial,
+         the_geom geometry(Polygon,3765)
+);
+
+INSERT INTO ST_MakeLine(the_geom)
+FROM points_ppi
+	
